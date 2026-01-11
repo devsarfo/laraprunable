2310a214f5e8b172b1db41ce7007accfb07e9c9f
Bernard Sarfo Twumasi <devsarfo@gmail.com>
2026-01-11 12:39:29 +0100

    Update PruneCommand functions

--- src/Console/Commands/PruneCommand.php
+++ src/Console/Commands/PruneCommand.php
@@ -2,13 +2,48 @@
 
 namespace DevSarfo\LaraPrunable\Console\Commands;
 
-use DevSarfo\LaraPrunable\Traits\SoftMassPrunable;
-use DevSarfo\LaraPrunable\Traits\SoftPrunable;
-use Illuminate\Database\Eloquent\MassPrunable;
-use Illuminate\Database\Eloquent\Prunable;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use InvalidArgumentException;
+use Symfony\Component\Finder\Finder;
 
 class PruneCommand extends \Illuminate\Database\Console\PruneCommand
 {
+    /**
+     * Determine the models that should be pruned.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    protected function models()
+    {
+        $models = $this->option('model');
+        $except = $this->option('except');
+
+        if ($models && $except) {
+            throw new InvalidArgumentException('The --models and --except options cannot be combined.');
+        }
+
+        if ($models) {
+            return (new Collection($models))
+                ->filter(static fn (string $model) => class_exists($model))
+                ->values();
+        }
+
+        return (new Collection(Finder::create()->in($this->getPath())->files()->name('*.php')))
+            ->map(function ($model) {
+                $namespace = $this->laravel->getNamespace();
+
+                return $namespace.str_replace(
+                        ['/', '.php'],
+                        ['\\', ''],
+                        Str::after($model->getRealPath(), realpath(app_path()).DIRECTORY_SEPARATOR)
+                    );
+            })
+            ->when(! empty($except), fn ($models) => $models->reject(fn ($model) => in_array($model, $except)))
+            ->filter(fn ($model) => $this->isPrunable($model))
+            ->values();
+    }
+
     /**
      * Determine if the given model class is prunable.
      *
@@ -20,10 +55,10 @@ class PruneCommand extends \Illuminate\Database\Console\PruneCommand
         $uses = class_uses_recursive($model);
 
         return ! empty(array_intersect($uses, [
-            Prunable::class,
-            MassPrunable::class,
-            SoftPrunable::class,
-            SoftMassPrunable::class,
+            \Illuminate\Database\Eloquent\Prunable::class,
+            \Illuminate\Database\Eloquent\MassPrunable::class,
+            \DevSarfo\LaraPrunable\Traits\SoftPrunable::class,
+            \DevSarfo\LaraPrunable\Traits\SoftMassPrunable::class,
         ]));
     }
 }
